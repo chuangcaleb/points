14528d45e1006edc5d4726166c8a4e7c2889f325
chuangcaleb <balechuang@gmail.com>
2022-09-28 15:53:50 +0800

    Use static abs root path + styled data view

--- counter/models.py
+++ counter/models.py
@@ -1,9 +1,13 @@
-from enum import unique
 from django.db import models
 
 # Create your models here.
 
 
 class PointsRecord(models.Model):
+
     group = models.CharField(max_length=200, primary_key=True)
-    points = models.IntegerField()
+    points = models.IntegerField(default=0, null=False)
+
+    @property
+    def uppercase_group(self):
+        return self.group.upper()
--- counter/views.py
+++ counter/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from .models import PointsRecord
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -7,4 +7,11 @@ from .models import PointsRecord
 def index(request):
     return render(request, "index.html", {
         'pointsrecords': PointsRecord.objects.all()
+    })
+
+
+def group_view(request, group):
+    return render(request, "group.html", {
+        'group': get_object_or_404(PointsRecord, pk=group)
+
     })
