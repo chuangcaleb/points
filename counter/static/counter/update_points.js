de23d760b0f2741d47dd550509c2a7eacca48fc9
chuangcaleb <balechuang@gmail.com>
2022-10-02 22:51:00 +0800

    Update variable points into input field

--- counter/static/counter/update_points.js
+++ counter/static/counter/update_points.js
@@ -7,12 +7,26 @@ document.addEventListener("DOMContentLoaded", function () {
   // Disable submit button by default:
   submit.disabled = true;
 
-  // Listen for input to be typed into the input field
-  input.onkeyup = () => {
+  function disable_empty_input() {
     if (input.value.length > 0) {
       submit.disabled = false;
     } else {
       submit.disabled = true;
     }
-  };
+  }
+
+  // Listen for input to be typed into the input field
+  input.onkeyup = () => disable_empty_input();
+
+  document.querySelectorAll(".fixed-values").forEach((button) => {
+    button.onclick = () => {
+      const input_value = document.getElementById("input");
+      if (input_value.value == "") {
+        input_value.value = 0;
+      }
+      input_value.value =
+        parseInt(input_value.value) + parseInt(button.innerHTML);
+      disable_empty_input();
+    };
+  });
 });
